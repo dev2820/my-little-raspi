7a9545d2dd6510a0a735a3c556f187f789c05ccf
dev2820 <second28200@gmail.com>
2021-05-01 15:25:22 +0900

    modify 라우터에서 비밀번호 관련된 부분 지움 (passcheck, passwordmodify로 옮길 예정)

--- backend/routes/usersController/modify.js
+++ backend/routes/usersController/modify.js
@@ -1,4 +1,3 @@
-const myhash = require('../../my_modules/pbkdf2');
 const mysqlDB = require('../../my_modules/mysql-db');
 
 /*  POST users modify */
@@ -6,19 +5,13 @@ module.exports = async function(req, res, next) {
 	const user_id = res.locals.userID;
     const user_name = req.body.name;
 	const user_email = req.body.email;
-    const user_password = req.body.password;
 	try {
         //open mariaDB
         const connection = await mysqlDB.getConnection(async conn => conn);
         //password change?
         const datas = [user_name+'',user_email+''];
-        if(user_password !== "") {
-            const [rows,fields] = await connection.query(`SELECT salt,password FROM users WHERE id LIKE ?`,[user_id+'']);
-            hashedPassword = await myhash.pbkdf2Hasing(user_password,rows[0].salt)
-            datas.push(hashedPassword);
-        }
 		datas.push(user_id+'');
-        const [rows,fields] = await connection.query(`UPDATE users SET name=?,email=?${user_password?',password=?':''} WHERE id=?`,datas);
+        const [rows,fields] = await connection.query(`UPDATE users SET name=?,email=? WHERE id=?`,datas);
         connection.release();
 		res.status(201).json({ message:'modify success'});
     }
