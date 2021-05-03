c2320096daee563a072999e26fdc69be09c7e0ca
dev2820 <second28200@gmail.com>
2021-05-03 19:59:14 +0900

    내가 만든 pbkdf2 모듈에 compare 함수 추가 및 makeSalt -> genSalt로 이름 변경

--- backend/my_modules/pbkdf2.js
+++ backend/my_modules/pbkdf2.js
@@ -2,12 +2,21 @@
 
 const crypto = require('crypto');
 const util = require('util');
-module.exports = {};
-module.exports.pbkdf2Hasing = async (password,salt) => {
+
+const pbkdf2Hasing = async (password,salt) => {
 	const asyncPbkdfs = util.promisify(crypto.pbkdf2);
     const key = await asyncPbkdfs(password,salt,100000,64,'sha512');
 	return key.toString('base64');
 }
-module.exports.makeSalt = async (password,salt) => {
+const genSalt = async (password,salt) => {
 	return await crypto.randomBytes(64).toString('base64')
 }
+const compare = async (plainPassword,password,salt) => {
+	const hashedPassword = await pbkdf2Hasing(plainPassword,salt);
+	return password === hashedPassword;
+}
+module.exports = {
+	pbkdf2Hasing,
+	genSalt,
+	compare
+};
