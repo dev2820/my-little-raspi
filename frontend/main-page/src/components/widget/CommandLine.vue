bb590e0b3c264e4163e7700a808282b959965a30
dev2820 <second28200@gmail.com>
2021-07-15 18:09:58 +0900

    commandLine 작동하도록 수정

--- frontend/main-page/src/components/widget/CommandLine.vue
+++ frontend/main-page/src/components/widget/CommandLine.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="command-line">
-        <div class="command">
-            $){{log.command}}
+        <div class="command" v-for="(command,index) in commands" :key="index">
+            <span class="prompt">{{index===0?'$':'>'}}</span>{{command}}
         </div>
         <div class="response" v-html="responseHtml"></div>
     </div>
@@ -16,15 +16,19 @@ export default {
             default() {
                 return {
                     command:'',
-                    response:''
+                    stdout:'',
+                    stderr:''
                 }
             },
             validator(value) {
-                return value['command'] && value['response'];
+                return value['command'] && (value['stdout'] || value['stderr']);
             }
         }
     },
     computed: {
+        commands() {
+            return this.log.command.split('\r\n')
+        },
         responseHtml(){
             return this.log.response.replace(/\n/g, "<br/>") 
         }
@@ -32,17 +36,27 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+*::selection {
+    color:#121212;
+    background: var(--primmary-color);
+}
 .command-line {
+    margin-bottom:10px;
     width:100%;
+    background-color: var(--background-color);
+    color: var(--foreground-color);
+    .command,.response {
+        text-align:left;
+        font-size:1.02rem;
+    }
+    .command {
+        .prompt {
+            color: var(--primmary-color);
+            margin-right:5px;
+        }
+    }
+    
 }
-.command,.response {
-    text-align:left;
-}
-textarea.response {
-    outline:none;
-    border:none;
-    resize:none;
-    width:100%;
-}
+
 </style>
